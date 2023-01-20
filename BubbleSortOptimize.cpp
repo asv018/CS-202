860e0116c4b988be4d54b685c56f49c875b8d085
Arpit Singh Verma <99022671+asv018@users.noreply.github.com>
2023-01-20 13:12:34 +0530

    Create BubbleSortOptimize.cpp

---
+++ BubbleSortOptimize.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int* arr, int size) {
+    bool flag = true;
+    for (int i = 0; i < size - 1 && flag; i++) {
+        flag = false;
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                flag = true;
+            }
+        }
+    }
+}
+
+int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int* arr = new int[size];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    bubbleSort(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    delete[] arr;
+    return 0;
+}
