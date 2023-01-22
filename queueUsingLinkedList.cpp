717896876fe47063e4139463fc85fa9f4f65fbdc
Arpit Singh Verma <99022671+asv018@users.noreply.github.com>
2023-01-22 10:54:29 +0530

    Create queueUsingLinkedList.cpp

---
+++ queueUsingLinkedList.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
+class Queue {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    Queue() {
+        head = NULL;
+        tail = NULL;
+    }
+
+    bool isEmpty() {
+        return head == NULL;
+    }
+
+    void enqueue(int data) {
+        Node* temp = new Node();
+        temp->data = data;
+        temp->next = NULL;
+
+        if (tail == NULL) {
+            head = tail = temp;
+            return;
+        }
+
+        tail->next = temp;
+        tail = temp;
+    }
+
+    int dequeue() {
+        if (isEmpty()) {
+            cout << "Queue is empty" << endl;
+            return INT_MIN;
+        }
+
+        Node* temp = head;
+        int data = temp->data;
+        head = head->next;
+
+        if (head == NULL) {
+            tail = NULL;
+        }
+
+        delete temp;
+        return data;
+    }
+};
