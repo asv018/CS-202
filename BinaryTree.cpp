2283c33a0624de65e6cb043a4d1168fb7b5027ba
Arpit Singh Verma <21bcs089@nith.ac.in>
2023-03-21 23:12:25 +0530

    BinaryTreeFileAdded1nodes

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -34,6 +34,7 @@ int main() {
     */
 
     TreeNode* root = new TreeNode(1);
+    // add the nodes
     root->left = new TreeNode(2);
     root->right = new TreeNode(3);
     root->left->left = new TreeNode(4);
